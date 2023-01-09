476bec87fd1a335a1770be5869708d7f32633c3c
Impulse3e <122300035+Impulse3e@users.noreply.github.com>
2023-01-09 19:57:39 +0300

    Add files via upload

---
+++ file.js
@@ -0,0 +1,14 @@
+function print_contact(){
+    let print_contact1 = "skype: helper"
+    let print_contact2 = "telegram: @helper"
+    let print_contact3 = "tel: 8995345235"
+    alert(print_contact1 + "\n" + print_contact2 + "\n" + print_contact3)
+}
+function f01(){
+    /*for(i = 0; i = 1; i++){
+        
+    }*/
+    a = document.getElementById("a01")
+    a.classList.toggle("animate__repeat-3")
+}
+setInterval(f01, 4000)
