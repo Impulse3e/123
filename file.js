8539db7c19b0d570cb43afe24a76997832935b41
Impulse3e <122300035+Impulse3e@users.noreply.github.com>
2023-03-13 19:08:54 +0300

    Add files via upload

--- file.js
+++ file.js
@@ -8,7 +8,63 @@ function f01(){
     /*for(i = 0; i = 1; i++){
         
     }*/
-    a = document.getElementById("a01")
+    let a = document.getElementById("a01")
     a.classList.toggle("animate__repeat-3")
 }
 setInterval(f01, 4000)
+function text_color(){
+    let a = document.getElementById("i01");
+    if(window.getComputedStyle(a).fontFamily == "Comfortaa"){
+    a.style.fontFamily = "Comic Sans MS";
+    }
+    if(window.getComputedStyle(a).fontFamily = "Comic Sans MS"){
+    a.style.fontFamily = "Comfortaa";
+    }
+}
+function text_roloc(){
+    let a = document.getElementById("a02")
+    let b = window.getComputedStyle(a).backgroundColor
+    /*if(b != "rgb(0, 0, 0)"){
+        a.style.backgroundColor = "black"
+    }
+    else{
+        a.style.backgroundColor = "white"
+    }*/
+    let a1 = parseFloat(prompt("Введите первое число"))
+    let a2 = parseFloat(prompt("Введите второе число"))
+    if(a1 > a2){
+        alert(a1);
+        a.style.backgroundColor = "black"
+    }
+    else{
+        alert(a2);
+        a.style.backgroundColor = "white";
+    }
+}
+function printnum(){
+    let a1 = parseFloat(prompt("Введите первое число"))
+    let a2 = parseFloat(prompt("Введите второе число"))
+    if(a1 > a2){
+        alert(a1);
+    }
+    else{
+        alert(a2);
+    }
+}
+function function1(){
+    let a = ["i01", "i02", "i03", "i04", "i05", "i06", "i07", "i08"]
+    for(let i = 0; i < a.length; i++){
+        let c = document.getElementById(a[i])
+        c.classList.toggle("c08_button")
+        c.classList.toggle("c08_button2")
+    }
+}
+function function2(){
+    let a = document.getElementById("i09")
+    if(a.style.display != 'none'){
+    a.style.display = 'none';
+    }
+    else{
+        a.style.display = 'block';
+    }
+}
---
+++ main.js
@@ -0,0 +1,64 @@
+function star(k){
+    let a = document.getElementById("io1")
+    let b = document.getElementById("io2")
+    let c = document.getElementById("io3")
+    let d = document.getElementById("io4")
+    let e = document.getElementById("io5")
+    if(k == 1){
+        a.src = "file/143945456406143d93e5d9.67304402star_1.png"
+    }
+    if(k == 2){
+        a.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        b.src = "file/143945456406143d93e5d9.67304402star_1.png"
+    }
+    if(k == 3){
+        a.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        b.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        c.src = "file/143945456406143d93e5d9.67304402star_1.png"
+    }
+    if(k == 4){
+        a.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        b.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        c.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        d.src = "file/143945456406143d93e5d9.67304402star_1.png"
+    }
+    if(k == 5){
+        a.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        b.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        c.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        d.src = "file/143945456406143d93e5d9.67304402star_1.png"
+        e.src = "file/143945456406143d93e5d9.67304402star_1.png"
+    }
+}
+function unstar(k){
+    let a = document.getElementById("io1")
+    let b = document.getElementById("io2")
+    let c = document.getElementById("io3")
+    let d = document.getElementById("io4")
+    let e = document.getElementById("io5")
+    if(k == 1){
+        a.src = "file/14394545640614572a5374.84355377star_2.png"
+    }
+    if(k == 2){
+        a.src = "file/14394545640614572a5374.84355377star_2.png"
+        b.src = "file/14394545640614572a5374.84355377star_2.png"
+    }
+    if(k == 3){
+        a.src = "file/14394545640614572a5374.84355377star_2.png"
+        b.src = "file/14394545640614572a5374.84355377star_2.png"
+        c.src = "file/14394545640614572a5374.84355377star_2.png"
+    }
+    if(k == 4){
+        a.src = "file/14394545640614572a5374.84355377star_2.png"
+        b.src = "file/14394545640614572a5374.84355377star_2.png"
+        c.src = "file/14394545640614572a5374.84355377star_2.png"
+        d.src = "file/14394545640614572a5374.84355377star_2.png"
+    }
+    if(k == 5){
+        a.src = "file/14394545640614572a5374.84355377star_2.png"
+        b.src = "file/14394545640614572a5374.84355377star_2.png"
+        c.src = "file/14394545640614572a5374.84355377star_2.png"
+        d.src = "file/14394545640614572a5374.84355377star_2.png"
+        e.src = "file/14394545640614572a5374.84355377star_2.png"
+    }
+}
